a82d96be79f76be32969b873934d059aa1d26a22
TomyRavn <godqwer123@naver.com>
2023-03-03 17:35:08 +0900

    Initial commit

---
+++ src/main/java/com/example/orderservice/service/OrderServiceImpl.java
@@ -0,0 +1,42 @@
+package com.example.orderservice.service;
+
+import com.example.orderservice.dto.OrderDto;
+import com.example.orderservice.jpa.OrderEntity;
+import com.example.orderservice.jpa.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+    private final OrderRepository orderRepository;
+
+    @Override
+    public OrderDto createOrder(OrderDto orderDetails) {
+        orderDetails.setOrderId(UUID.randomUUID().toString());
+        orderDetails.setTotalPrice(orderDetails.getQty() * orderDetails.getUnitPrice());
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        OrderEntity orderEntity = mapper.map(orderDetails, OrderEntity.class);
+        orderRepository.save(orderEntity);
+        OrderDto returnValue = mapper.map(orderEntity, OrderDto.class);
+        return returnValue;
+    }
+
+    @Override
+    public OrderDto getOrderByOrderId(String orderId) {
+        OrderEntity orderEntity = orderRepository.findByOrderId(orderId);
+        OrderDto orderDto = new ModelMapper().map(orderEntity, OrderDto.class);
+        return orderDto;
+    }
+
+    @Override
+    public Iterable<OrderEntity> getOrdersByUserId(String userId) {
+        return orderRepository.findByUserId(userId);
+    }
+}
